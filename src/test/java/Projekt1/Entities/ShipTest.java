ef4dc8b575350586d3ac1796eb41a837cb68ff27
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-06 01:23:34 +0200

    Introduce AAA pattern + AssertJ assertions

--- src/test/java/Projekt1/Entities/ShipTest.java
+++ src/test/java/Projekt1/Entities/ShipTest.java
@@ -1,14 +1,20 @@
 package Projekt1.Entities;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
 
 public class ShipTest
 {
     @Test
     public void newShip_FacesNorthDirection_ByDefault()
     {
+        // Arrange
         Ship ship = new Ship();
-        assertEquals(Direction.North, ship.getDirection());
+
+        // Act
+
+        // Assert
+        assertThat(ship.getDirection()).isEqualTo(Direction.North);
     }
 }
