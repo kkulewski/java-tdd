953592a338e888240d309b08186a65bfbf875dfe
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-06 01:48:49 +0200

    Refactor app class + remove sample test

--- src/test/java/Projekt1/AppTest.java
+++ src/test/java/Projekt1/AppTest.java
@@ -1,13 +1,5 @@
 package Projekt1;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
 public class AppTest
 {
-    @Test
-    public void testApp()
-    {
-        assertTrue(true);
-    }
 }
