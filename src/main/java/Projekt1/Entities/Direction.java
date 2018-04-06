64c1eaae8eb874cd512ba2866954777975efe45d
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-06 04:12:28 +0200

    Add direction right rotation implementation

--- src/main/java/Projekt1/Entities/Direction.java
+++ src/main/java/Projekt1/Entities/Direction.java
@@ -1,9 +1,27 @@
 package Projekt1.Entities;
 
+import lombok.Getter;
+
 public enum Direction
 {
-    North,
-    East,
-    South,
-    West
+    North(0),
+    East(1),
+    South(2),
+    West(3);
+
+    @Getter
+    private int index;
+
+    Direction(int index)
+    {
+        this.index = index;
+    }
+
+    private static Direction[] directions = Direction.values();
+
+    public static Direction Right(Direction direction)
+    {
+        int index = (direction.index + 1) % directions.length;
+        return directions[index];
+    }
 }
