8bbaf55fcac6a19d23375080efe3d0a64fec1da7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-07 01:02:20 +0200

    Add out of map bound field access tests

---
+++ src/test/java/Projekt1/Entities/MapTest.java
@@ -0,0 +1,40 @@
+package Projekt1.Entities;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MapTest
+{
+    @Test
+    public void getField_GivenInvalidRowIndex_ThrowsIndexOutOfBoundsException()
+    {
+        // Arrange
+        Map map = new Map(1);
+
+        // Act & Assert
+        assertThatThrownBy(() -> map.getField(1,0))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    public void getField_GivenInvalidColumnIndex_ThrowsIndexOutOfBoundsException()
+    {
+        // Arrange
+        Map map = new Map(1);
+
+        // Act & Assert
+        assertThatThrownBy(() -> map.getField(0,1))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    public void getField_GivenInvalidRowAndColumnIndex_ThrowsIndexOutOfBoundsException()
+    {
+        // Arrange
+        Map map = new Map(1);
+
+        // Act & Assert
+        assertThatThrownBy(() -> map.getField(1,1))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+}
