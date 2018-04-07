0181d0905c41d7f572cb7e3a49a9680481b6e603
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-07 23:44:07 +0200

    Add command processor impl

---
+++ src/main/java/Projekt1/Logic/CommandProcessor.java
@@ -0,0 +1,50 @@
+package Projekt1.Logic;
+
+import Projekt1.Logic.Interfaces.ICommandProcessor;
+import Projekt1.Logic.Interfaces.IMoveService;
+import Projekt1.Logic.Interfaces.IActionResult;
+
+public class CommandProcessor implements ICommandProcessor
+{
+    private IMoveService moveService;
+
+    public CommandProcessor(IMoveService moveService)
+    {
+        this.moveService = moveService;
+    }
+
+    public IActionResult execute(char symbol)
+    {
+        IActionResult result;
+
+        char command = Character.toLowerCase(symbol);
+        switch (command)
+        {
+            case 'l':
+                result = moveService.turnLeft();
+                break;
+
+            case 'r':
+                result = moveService.turnRight();
+                break;
+
+            case 'n':
+                result = moveService.moveForward();
+                break;
+
+            case 'w':
+                result = moveService.moveBack();
+                break;
+
+            case 's':
+                result = moveService.shoot();
+                break;
+
+            default:
+                result = IActionResult.getErrorResult();
+                break;
+        }
+
+        return result;
+    }
+}
