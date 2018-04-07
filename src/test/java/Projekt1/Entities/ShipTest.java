5b7f75a0f1304335764161e7df78ba7c1fbba2b6
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-07 17:23:18 +0200

    Add custom ship direction + coords tests

--- src/test/java/Projekt1/Entities/ShipTest.java
+++ src/test/java/Projekt1/Entities/ShipTest.java
@@ -37,4 +37,34 @@ public class ShipTest
         // Act & Assert
         assertThat(ship.getY()).isEqualTo(0);
     }
+
+    @Test
+    public void newShip_WithCustomDirectionSouth_FacesSouth()
+    {
+        // Arrange
+        Ship ship = new Ship(Direction.South, 0, 0);
+
+        // Act & Assert
+        assertThat(ship.getDirection()).isEqualTo(Direction.South);
+    }
+
+    @Test
+    public void newShip_WithCustomX5_XCoordinate_Is5()
+    {
+        // Arrange
+        Ship ship = new Ship(Direction.North, 5, 0);
+
+        // Act & Assert
+        assertThat(ship.getX()).isEqualTo(5);
+    }
+
+    @Test
+    public void newShip_WithCustomY5_YCoordinate_Is5()
+    {
+        // Arrange
+        Ship ship = new Ship(Direction.North, 0, 5);
+
+        // Act & Assert
+        assertThat(ship.getY()).isEqualTo(5);
+    }
 }
