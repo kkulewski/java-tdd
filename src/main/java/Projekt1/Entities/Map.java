dc7f1be441d33fbd130152ab5f08b3ebe0a8d6ac
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-07 03:56:05 +0200

    Add map with islands impl

--- src/main/java/Projekt1/Entities/Map.java
+++ src/main/java/Projekt1/Entities/Map.java
@@ -1,5 +1,7 @@
 package Projekt1.Entities;
 
+import java.util.Random;
+
 public class Map
 {
     private Field[][] fields;
@@ -11,6 +13,15 @@ public class Map
         this.fields = new Field[size][size];
 
         fillWithWater();
+    }
+
+    public Map(int size, boolean withLand)
+    {
+        this(size);
+        if (withLand)
+        {
+            addIslands();
+        }
     }
 
     public Field getField(int row, int column)
@@ -25,6 +36,25 @@ public class Map
             for (int j = 0; j < this.size; j++)
             {
                 this.fields[i][j] = Field.Water;
+            }
+        }
+    }
+
+    private void addIslands()
+    {
+        Random random = new Random();
+        int islandsAdded = 0;
+        int islandsToAdd = (this.size * this.size) / 10;
+
+        while (islandsAdded < islandsToAdd)
+        {
+            int row = random.nextInt(this.size);
+            int col = random.nextInt(this.size);
+
+            if (this.fields[row][col] != Field.Land)
+            {
+                this.fields[row][col] = Field.Land;
+                islandsAdded++;
             }
         }
     }
