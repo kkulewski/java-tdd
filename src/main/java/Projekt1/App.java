a4b04e6f952361c47b3e08d2d926b46f8230d026
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-08 05:22:38 +0200

    Add simple game demo

--- src/main/java/Projekt1/App.java
+++ src/main/java/Projekt1/App.java
@@ -4,6 +4,12 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Game game = new Game(12);
+        while (true)
+        {
+            System.out.println("Enter command chain:");
+            String input = System.console().readLine();
+            game.processCommandChain(input);
+        }
     }
 }
