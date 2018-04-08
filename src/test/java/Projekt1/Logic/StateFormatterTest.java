34270817b145bb00a099f70fefe1e3df290420fc
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-08 03:05:06 +0200

    Add get current state result test

---
+++ src/test/java/Projekt1/Logic/StateFormatterTest.java
@@ -0,0 +1,35 @@
+package Projekt1.Logic;
+
+import static org.assertj.core.api.Assertions.*;
+
+import Projekt1.Entities.*;
+import Projekt1.Logic.Interfaces.IStateFormatter;
+import org.junit.jupiter.api.Test;
+
+public class StateFormatterTest
+{
+    @Test
+    public void getFormattedState()
+    {
+        // Arrange
+        Field[] row1 = { Field.Water, Field.Water, Field.Water };
+        Field[] row2 = { Field.Water, Field.Land, Field.Land };
+        Field[] row3 = { Field.Water, Field.Water, Field.Land };
+        Field[][] fields = { row1, row2, row3 };
+        Map map = new Map(fields);
+        Ship ship = new Ship(Direction.North, new Coordinate(0, 0));
+        IStateFormatter sf = new StateFormatter(map, ship);
+
+        // Act
+        String result = sf.getCurrentState();
+
+        // Assert
+        StringBuilder sb = new StringBuilder();
+        sb.append("   "); sb.append(System.lineSeparator());
+        sb.append(" ##"); sb.append(System.lineSeparator());
+        sb.append("  #"); sb.append(System.lineSeparator());
+        String expected = sb.toString();
+
+        assertThat(result).contains(expected);
+    }
+}
