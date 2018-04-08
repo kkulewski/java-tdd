83fc6e2b313aa1de38189a0089686cca1ac51681
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-08 03:05:18 +0200

    Add state formatter impl

---
+++ src/main/java/Projekt1/Logic/StateFormatter.java
@@ -0,0 +1,68 @@
+package Projekt1.Logic;
+
+import Projekt1.Entities.*;
+import Projekt1.Logic.Interfaces.IStateFormatter;
+
+public class StateFormatter implements IStateFormatter
+{
+    private Map map;
+    private Ship ship;
+
+    public StateFormatter(Map map, Ship ship)
+    {
+        this.map = map;
+        this.ship = ship;
+    }
+
+    @Override
+    public String getCurrentState()
+    {
+        Coordinate shipCoordinate = ship.getCoordinate();
+        char shipArrow = getShipDirectionArrow();
+
+        int mapSize = map.getSize();
+        StringBuilder sb = new StringBuilder();
+
+        for (int x = 0; x < mapSize; x++)
+        {
+            for (int y = 0; y < mapSize; y++)
+            {
+                Coordinate c = new Coordinate(x, y);
+                if (c == shipCoordinate)
+                {
+                    sb.append(shipArrow);
+                }
+                else
+                {
+                    Field f = map.getField(c);
+                    sb.append(f.getSymbol());
+                }
+            }
+
+            sb.append(System.lineSeparator());
+        }
+
+        sb.append(System.lineSeparator());
+        return sb.toString();
+    }
+
+    private char getShipDirectionArrow()
+    {
+        Direction shipDirection = ship.getDirection();
+        char arrow = ' ';
+
+        if (shipDirection == Direction.North)
+            arrow = '^';
+
+        if (shipDirection == Direction.East)
+            arrow = '>';
+
+        if (shipDirection == Direction.South)
+            arrow = 'V';
+
+        if (shipDirection == Direction.West)
+            arrow = '<';
+
+        return arrow;
+    }
+}
