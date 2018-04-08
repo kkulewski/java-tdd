85c6d1b72bbbd53611a302cb771c7ea97e858d23
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-04-08 03:27:41 +0200

    Add execute with commands from CSV test + example test data

--- src/test/java/Projekt1/Logic/CommandProcessorTest.java
+++ src/test/java/Projekt1/Logic/CommandProcessorTest.java
@@ -1,6 +1,7 @@
 package Projekt1.Logic;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import Projekt1.Logic.Stubs.MoveServiceStub;
 import Projekt1.Logic.Interfaces.IActionResult;
@@ -9,6 +10,8 @@ import Projekt1.Logic.Interfaces.IMoveService;
 import org.hamcrest.core.Every;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,5 +56,20 @@ public class CommandProcessorTest
 
         // Assert
         Assert.assertThat(results, Every.everyItem(hasProperty("status", equalTo(false))));
+    }
+
+    @ParameterizedTest(name = "{index} => command={0}, result={1}")
+    @CsvFileSource(resources = "/commands.csv")
+    public void execute_WithCommandsFromCsv_ReturnsExpectedResults(char command, boolean result)
+    {
+        // Arrange
+        IMoveService ms = new MoveServiceStub();
+        ICommandProcessor cp = new CommandProcessor(ms);
+
+        // Act
+        IActionResult actionResult = cp.execute(command);
+
+        // Assert
+        assertEquals(actionResult.getStatus(), result);
     }
 }
